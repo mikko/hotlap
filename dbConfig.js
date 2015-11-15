8e0d9d5238f7aee08bc4c105fb29dd901afc0dac
Mikko Pohja <mikko.pohja@futurice.com>
2015-11-15 22:07:52 +0200

    Option for using separate configuration for production database

--- dbConfig.js
+++ dbConfig.js
@@ -1,2 +1,11 @@
-var connectionString = process.env.DATABASE_URL || 'postgres://postgres:hotlap@localhost:32768/postgres';
+var fs = require("fs");
+var prodConfig;
+try{
+	prodConfig = fs.readFileSync("prodDB.conf").toString();
+}
+catch(e) {
+	console.log("Database url for production not found");
+}
+fs.stat
+var connectionString = prodConfig || 'postgres://postgres:hotlap@localhost:32768/postgres';
 module.exports = connectionString;
