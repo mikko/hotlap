c8145a41abe2a38a8cc47603d2a45e37aa3b53b6
Miro Nieminen <miro.nieminen@futurice.com>
2015-11-19 23:21:15 +0200

    Look for Postgres from the default port with hotlap/hotlap/hotlap -config

--- dbConfig.js
+++ dbConfig.js
@@ -1,11 +1,13 @@
 var fs = require("fs");
+
 var prodConfig;
-try{
-	prodConfig = fs.readFileSync("prodDB.conf").toString();
+try {
+    prodConfig = fs.readFileSync("prodDB.conf").toString();
 }
 catch(e) {
-	console.log("Database url for production not found");
+    console.log("Database url for production not found");
 }
 fs.stat
-var connectionString = prodConfig || 'postgres://postgres:hotlap@localhost:32768/postgres';
+var connectionString = prodConfig || 'postgres://hotlap:hotlap@localhost:5432/hotlap';
+
 module.exports = connectionString;
